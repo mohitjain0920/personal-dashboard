c40cc22f9cba03549f54f72a66d4f47610fcaf03
mohitjain0920 <mohitjain0920@gmail.com>
2022-07-19 16:19:30 +0530

    time change again

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ fetch("https://api.coingecko.com/api/v3/coins/ethereum")
 
 function getCurrentTime() {
     const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "medium"})
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
 }
 
 setInterval(getCurrentTime, 1000)
